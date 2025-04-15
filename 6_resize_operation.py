f8e13bc539d01a2c4a5833f4a917e3dadf282f02
ADIVI SRI LAKSHMI SREEYA <11229a002@kanchiuniv.ac.in>
2025-04-15 23:34:16 +0530

    6_resize_operation.py

--- 6_resize_operation.py
+++ 6_resize_operation.py
@@ -3,12 +3,10 @@ image = cv2.imread('image.png')
 cv2.imshow('Original Image', image)
 height = image.shape[0]
 width = image.shape[1]
-print("The Dimensions of the image are: ", height,
-width)
+print("The Dimensions of the image are: ", height,width)
 new_width = 400
 new_height = 300
-resized_image = cv2.resize(image, (new_height,
-new_width), interpolation=cv2.INTER_LINEAR)
+resized_image = cv2.resize(image, (new_height,new_width), interpolation=cv2.INTER_LINEAR)
 cv2.imwrite('Resized_Image.jpg', resized_image)
 cv2.imshow('Resized_Image', resized_image)
 height = resized_image.shape[0]
